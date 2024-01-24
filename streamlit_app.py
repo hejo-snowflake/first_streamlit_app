50e6c28239c471e53699d2999615dc9fac549083
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-24 14:29:40 +0100

    Update streamlit_app.py
    Changed widget such that only fruits are shown which have not been selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')  # use the Fruit column as the index so that it appears in the selection widget below (instead of just IDs)
 
 # add widget for fruit selection
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display table
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
