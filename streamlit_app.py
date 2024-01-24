355105e8a6165e7403dddbb2bdbaf617a062dfd3
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-24 14:27:49 +0100

    Update streamlit_app.py
    Added "Avocado" and "Strawberries" as default for fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')  # use the Fruit column as the index so that it appears in the selection widget below (instead of just IDs)
 
 # add widget for fruit selection
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 # display table
 streamlit.dataframe(my_fruit_list)
