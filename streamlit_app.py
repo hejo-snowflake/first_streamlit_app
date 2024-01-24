5a976ba2235a09adaecd6d68766c7d8d9d789bcf
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-24 14:23:23 +0100

    Update streamlit_app.py
    Added widget for fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,4 +15,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # import table with data on fruits
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# add widget for fruit selection
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# display table
 streamlit.dataframe(my_fruit_list)
