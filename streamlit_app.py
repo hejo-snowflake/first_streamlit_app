8f6b3b756f13fb778bd6e53c34f4454575f246dd
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-25 11:39:35 +0100

    Update streamlit_app.py
    Replaced "from streamlit" additions to DB with proper one

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ if streamlit.button('Get Fruit Load List'):
 # Allow the end user to add fruit to the list, still adds "from streamlit" as fruit
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("INSERT INTO fruit_load_list VALUES ('from streamlit')")
+        my_cur.execute("INSERT INTO fruit_load_list VALUES ('" + new_fruit + "')")
         return 'Thanks for adding ' + new_fruit
     
 # add button for adding fruit
