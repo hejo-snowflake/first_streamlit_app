d48ccb3104c74f019801c21156170122b1740948
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-25 11:30:48 +0100

    Update streamlit_app.py
    Created function for call to Snowflake DB
    Added button for loading the fruit list from the DB

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,17 +50,21 @@ except URLError as e:
 
 
 
-# FOR TROUBLESHOOTING: dont't run code after this statement
-streamlit.stop()
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-# my_data_row  = my_cur.fetchone() # one line  --> this HAS TO BE COMMENTED OUT (or removed), otherwise the first row is saved here and thus not included in the my_data_rows
-my_data_rows = my_cur.fetchall() # all lines
+
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+# Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * FROM fruit_load_list")
+        return my_cur.fetchall() # all lines
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 
 # Allow the end user to add fruit to the list (not really though, it's just displayed but not added to the DB
