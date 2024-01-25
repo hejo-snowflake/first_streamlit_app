b25e7b875c02cb186ef8dddb2ee4a462517a08aa
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-25 11:43:27 +0100

    Update streamlit_app.py
    Changed fruit list header
    added close() statements for snowflake connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ except URLError as e:
 
 
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 # Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -64,6 +64,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 
@@ -79,5 +80,6 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     streamlit.text( insert_row_snowflake( add_my_fruit ) )
+    my_cnx.close()
 
 
