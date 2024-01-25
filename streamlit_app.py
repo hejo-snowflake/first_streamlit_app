e91bd33178d13b29030edcea7d856b90b2ca5d78
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-25 08:50:24 +0100

    Update streamlit_app.py
    Removed raw JSON output

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())  # just writes the data to the screen
 
 # take the json data and normalize it, i.e. turns it into a proper table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
