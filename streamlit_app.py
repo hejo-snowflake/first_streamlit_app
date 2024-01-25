c68338ed35292cba40a28fb4396967426e9ebfb4
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-25 08:38:10 +0100

    Update streamlit_app.py
    Added New Section to display fruityvice API response

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display table
 streamlit.dataframe(fruits_to_show)
+
+
+
+# New Section to display fruityvice API response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
