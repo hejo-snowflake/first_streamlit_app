fdad00b1426d5f1ce1aee22d8bd5eb04b86c813e
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-25 10:56:57 +0100

    Update streamlit_app.py
    Added an input field where users can enter fruit they would like to add. For now, this is just a gimmick though and not actually connected to the DataBase.

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,3 +48,8 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall() # all lines
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+
+# Allow the end user to add fruit to the list (not really though, it's just displayed but not added to the DB
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
