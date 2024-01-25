7979b0fff0eeeaf1a032c27d0542b11d4d0e75bf
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-25 11:11:54 +0100

    Update streamlit_app.py
    Cleaned up import statements, old ones left as comments but could be removed, also added urllib import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 # import and set up streamlit app
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+import urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Diner')
 
 # add some basic headings and items
@@ -13,7 +18,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 # import table with data on fruits
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')  # use the Fruit column as the index so that it appears in the selection widget below (instead of just IDs)
 
@@ -31,7 +36,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')  # added input text field with 'Kiwi' as default
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # take the json data and normalize it, i.e. turns it into a proper table
@@ -39,8 +44,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the normalized data as a table
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
 
+# FOR TROUBLESHOOTING: dont't run code after this statement
+streamlit.stop()
+
+
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
