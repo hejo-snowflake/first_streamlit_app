343d4307f770cda85440e38f03f9b45652a19cca
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-25 10:50:24 +0100

    Update streamlit_app.py
    Commented out my_data_row to fix missing banana item in list

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_row  = my_cur.fetchone() # one line
+# my_data_row  = my_cur.fetchone() # one line  --> this HAS TO BE COMMENTED OUT (or removed), otherwise the first row is saved here and thus not included in the my_data_rows
 my_data_rows = my_cur.fetchall() # all lines
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
