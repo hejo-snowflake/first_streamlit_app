fabff8e6a8a763cdefd1ff742915f6e421748e29
hejo-snowflake <157604264+hejo-snowflake@users.noreply.github.com>
2024-01-25 10:21:25 +0100

    Update streamlit_app.py
    Added import for snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the normalized data as a table
 streamlit.dataframe(fruityvice_normalized)
 
+import snowflake.connector
